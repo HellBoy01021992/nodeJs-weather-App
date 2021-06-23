8fa81e81e0c47a6be86c98b4fd881940690a0eb1
Subhradip <subhradip@gozo.cab>
2021-06-24 03:56:57 +0530

    Setup App for Heroku

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
-const app = express()
+const path      = require('path')
+const express   = require('express')
+const hbs       = require('hbs')
+const geocode   = require('./utils/geocode')
+const forecast  = require('./utils/forecast')
 
+const app  = express()
+const port = process.env.PORT || 3000
 //Setup paths for Express config
 const publicDirectory = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -103,7 +104,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log ('Server is up')
 })
 
